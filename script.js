83d28f0ff714562fd82789bd93a87893020f87c0
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-12 14:21:54 -0600

    Winning number generating. Correct guess logic functioning. Onclick handler functioning.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+console.log(number);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  }
+});
