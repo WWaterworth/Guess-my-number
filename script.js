f9f9bda9d292ad7558813c41cd8311100e4af35d
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-12 21:04:49 -0600

    Score cant be reduced to zero. Background color changes on correct guess

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -27,6 +30,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = 'You lose!';
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too low!';
