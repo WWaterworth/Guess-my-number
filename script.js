26f5fbc8863d00dbb3d0d3e1bc1c759da66c6018
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-13 06:13:34 -0600

    Play again button resets state to allow another game to be played

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
 let score = 20;
 let highScore = 0;
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.guess').value = null;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
