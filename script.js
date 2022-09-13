7a9e5a877aff41fc5ef8a75ba479fa2ca0d2c8ec
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-12 20:15:01 -0600

    Message changes in DOM on guess of high/low/correct. Score is reduced on incorrect guess

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 
 // document.querySelector('.guess').value = 23;
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
 
-console.log(number);
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,5 +19,21 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose!';
+    }
   }
 });
